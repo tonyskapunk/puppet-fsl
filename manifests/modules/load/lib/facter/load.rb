0c5f85ecdcde5aaf3ce65d13fe22c9055c03f81d
tonyskapunk <tonysk8@gmx.net>
2011-11-02 19:04:28 +0000

    Fix facter load.

--- manifests/modules/load/lib/facter/load.rb
+++ manifests/modules/load/lib/facter/load.rb
@@ -3,8 +3,9 @@ sysloads = IO.readlines('/proc/loadavg')[0].split.reverse
 factnames = [ "load1", "load5", "load15" ]
 factnames.each do |factname|
   Facter.add(factname) do
+    myload = sysloads.pop.to_s.chomp
     setcode do
-      sysloads.pop.to_s.chomp
+      myload
     end
   end
 end
